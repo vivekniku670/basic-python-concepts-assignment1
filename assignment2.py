c285ad3c80bdd0edc00192d373a9e03ceb6cafcb
vivekniku670 <vivekniku670@gmail.com>
2025-08-21 15:42:21 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,11 @@
+# Task 2: Personalized Greeting
+
+# Taking input
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+
+# Creating full name
+full_name = first_name + " " + last_name
+
+# Printing greeting
+print(f"Hello, {full_name}! Welcome to Python programming 🚀")
